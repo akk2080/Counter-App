3d778a7c817b66ccd4772ed2768cd23a9e70f674
AAKARSH <akk2080@gmail.com>
2024-07-29 17:31:16 +0530

    main funtionality added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
+import './components/Style/CounterButton.css'
 
 function App() {
   return (
     <div className="App">
      <h1>Counter Application</h1>
+     <br></br>
+     <Counter/>
     </div>
   );
 }
